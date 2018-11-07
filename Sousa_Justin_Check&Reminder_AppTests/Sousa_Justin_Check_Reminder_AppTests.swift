affa8e6accf32808000b60743674f109e01e84b5
Justin Sousa <justinsousa@justins-macbook-pro.local>
2018-11-07 12:03:28 -0500

    Initial Commit

---
+++ Sousa_Justin_Check&Reminder_AppTests/Sousa_Justin_Check_Reminder_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Sousa_Justin_Check_Reminder_AppTests.swift
+//  Sousa_Justin_Check&Reminder_AppTests
+//
+//  Created by Justin Sousa on 2018-11-07.
+//  Copyright © 2018 Justin Sousa. All rights reserved.
+//
+
+import XCTest
+@testable import Sousa_Justin_Check_Reminder_App
+
+class Sousa_Justin_Check_Reminder_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
